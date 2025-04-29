402a18741db5d402a3d46646e76f5df847dc64ee
michael120288 <78063023+michael120288@users.noreply.github.com>
2025-04-28 23:37:15 -0400

    created two test with .then and with .end (#1)
    Done

--- src/tests/log_in.spec.ts
+++ src/tests/log_in.spec.ts
@@ -1,5 +1,5 @@
 import { User } from "../../helper/interface";
-import { deleteFunction, getUser, login, login2, signUp } from "../../helper/user";
+import { deleteFunction, deleteFunction2, getUser, login, login2, signUp, signUp2 } from "../../helper/user";
 import * as supertest from "supertest";
 const request = supertest("http://localhost:8001/api/v1");
 
@@ -32,6 +32,63 @@ describe("USER SIGNUP AND LOGIN", () => {
         console.error("Error during sign up:", error);
         throw error; // Rethrow the error to fail the test
       }
+    });
+    it.only("should signup , login and delete the user using .then()", () => {
+      return signUp(user)
+        .then((res) => {
+          console.log(res.body);
+          expect(res.statusCode).toBe(201);
+          expect(res.body.data.user.email).toEqual(user.email);
+          expect(res.body.status).toEqual("success");
+          return login(user);
+        })
+        .then((loginRes) => {
+          console.log(loginRes.body, "loginRes");
+          expect(loginRes.statusCode).toBe(200);
+          expect(loginRes.body.status).toBe("success");
+          console.log("cookie", loginRes.headers["set-cookie"][0]);
+          cookie = loginRes.headers["set-cookie"][0].split(";")[0];
+          return deleteFunction(cookie);
+        })
+        .then((deleteRes) => {
+          expect(deleteRes.statusCode).toBe(200);
+          expect(deleteRes.body.message).toBe("User deleted successfully");
+          return login(user);
+        })
+        .then((loginAfterDelete) => {
+          expect(loginAfterDelete.statusCode).toBe(401);
+          expect(loginAfterDelete.body.message).toBe("Incorrect email or password");
+        });
+    });
+    it.only("should signup , login and delete the user using .end(done callback)", (done) => {
+      signUp2(user).end((err, res) => {
+        if (err) return done(err);
+        console.log(res.body);
+        expect(res.statusCode).toBe(201);
+        expect(res.body.data.user.email).toEqual(user.email);
+
+        login2(user).end((err, loginRes) => {
+          if (err) return done(err);
+
+          expect(loginRes.statusCode).toBe(200);
+          expect(loginRes.body.status).toBe("success");
+          console.log("cookie", loginRes.headers["set-cookie"][0]);
+          cookie = loginRes.headers["set-cookie"][0].split(";")[0];
+          return deleteFunction2(cookie).end((err, deleteRes) => {
+            if (err) return done(err);
+
+            expect(deleteRes.statusCode).toBe(200);
+            expect(deleteRes.body.message).toBe("User deleted successfully");
+            login2(user).end((err, loginAfterDelete) => {
+              if (err) return done(err);
+
+              expect(loginAfterDelete.statusCode).toBe(401);
+              expect(loginAfterDelete.body.message).toBe("Incorrect email or password");
+              done();
+            });
+          });
+        });
+      });
     });
   });
 });
